f2ff002da03df173187a340138a2344f9e9b4ca9
dimmalex <dimmalex@gmail.com>
2022-10-17 22:09:35 +0800

    network mode depend on network@frame configure

--- project/netcard/device/netcard.c
+++ project/netcard/device/netcard.c
@@ -147,11 +147,56 @@ boole_t _down( obj_t this, param_t param )
 }
 talk_t _connect( obj_t this, param_t param )
 {
-    return ttrue;
+	talk_t v;
+	const char *object;
+	const char *ifname;
+
+	object = obj_combine( this );
+	v = param_talk( param, 1 );
+	ifname = json_string( v, "ifname" );
+	/* start the keeplive */
+	if ( ifname != NULL && strstr( ifname, WAN_COM ) != NULL )
+	{
+		/* start the ifdev keeplive */
+		sstop( "%s-keeplive", object );
+	}
+
+	return ttrue;
 }
 boole_t _connected( obj_t this, param_t param )
 {
-    return ttrue;
+	talk_t v;
+	talk_t ret;
+	const char *object;
+	const char *netdev;
+	const char *ifname;
+
+	object = obj_combine( this );
+
+	ret = ttrue;
+	v = param_talk( param, 1 );
+	ifname = json_string( v, "ifname" );
+	/* start the keeplive */
+	if ( ifname != NULL && strstr( ifname, WAN_COM ) != NULL )
+	{
+		/* get the netdev */
+		netdev = register_pointer( object, "netdev" );
+		if ( netdev == NULL || *netdev == '\0' )
+		{
+			return tfalse;
+		}
+		/* check the connect ok */
+		if ( netdev_flags( netdev, IFF_RUNNING ) > 0 )
+		{
+			ret = ttrue;
+		}
+		else
+		{
+			ret = tfalse;
+		}
+	}
+
+	return ret;
 }
 talk_t _status( obj_t this, param_t param )
 {
@@ -159,8 +204,64 @@ talk_t _status( obj_t this, param_t param )
 }
 talk_t _online( obj_t this, param_t param )
 {
+	const char *object;
+	const char *ifname;
+	char name[NAME_MAX];
+	
+	object = obj_combine( this );
+	ifname = param_string( param, 1 );
+
+	/* start the keeplive */
+	if ( ifname != NULL && strstr( ifname, WAN_COM ) != NULL )
+	{
+		/* start the ifdev keeplive */
+		snprintf( name, sizeof(name), "%s-keeplive", object );
+		sstarts( name, object, "keeplive", ifname );
+	}
+
 	return ttrue;
 }
+talk_t _keeplive( obj_t this, param_t param )
+{
+	int i;
+	talk_t v;
+    const char *object;
+	const char *ifname;
+
+	object = obj_combine( this );
+	ifname = param_string( param, 1 );
+
+	/* check and check forever */
+	v = json_create( NULL );
+	json_set_string( v, "ifname", ifname );
+	i = 0;
+	while( 1 )
+	{
+		if ( scallt( object, "connected", v ) == ttrue )
+		{
+			sleep( 5 );
+		}
+		else
+		{
+			if ( i >= 10 )
+			{
+				info(  "%s(%s) connection lost", ifname, object );
+				break;
+			}
+			else
+			{
+				debug( "%s(%s) connection broke", ifname, object );
+				i++;
+				usleep( 200000 );
+			}
+		}
+	}
+	talk_free( v );
+	sreset( NULL, NULL, NULL, ifname );
+
+	return ttrue;
+}
+
 
 
 
