438136138dd488c5e2f0aa87ad931381ec4da109
dimmalex <dimmalex@gmail.com>
2023-04-24 11:08:19 +0800

    add the sline depend for libreadline

---
+++ project/tui/sline/slink.c
@@ -0,0 +1,52 @@
+/*
+ *    Description:  command component call depend on sline
+ * 		Author:  dimmalex (dim), dimmalex@gmail.com
+ * 	   Company:  ASHYELF
+ */
+#include "land/skin.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+
+int main( int argc, const char **argv )
+{
+	char *line;
+	struct stat st;
+
+	if ( stat( "/etc/banner.he", &st ) == 0 )
+	{
+		shell( "cat /etc/banner.he" );
+	}
+	while (1)
+	{
+		line = readline("# ");
+		if ( line == NULL )
+		{
+			continue;
+		}
+		if ( *line == '\0' || *line == '\r' || *line == '\n' )
+		{
+			free( line );
+			continue;
+		}
+		if ( 0 == strncmp( line, "elf", 3 ) )
+		{
+			/* open the new sessoin */
+			execl( "/bin/ash", "/bin/ash", "--login", NULL );
+			break;
+		}
+		else if ( 0 == strncmp( line, "exit", 4 ) )
+		{
+			break;
+		}
+		add_history( line );
+		string_he_command( line );
+		free( line );
+	}
+
+	return 0;
+}
+
+
+
--- project/tui/sline/strlcpy.h
+++
@@ -1,3 +0,0 @@
-/* See LICENSE file for copyright and license details. */
-
-size_t strlcpy(char *dst, const char *src, size_t siz);
